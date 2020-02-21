02cd1ab7ddf9821e4c1bb9ce893815b31f44d761
Piotr Grzelczak <piotr.karol.grzelczak@gmail.com>
2020-02-21 23:36:00 +0100

    Update .calculateHandValue()

--- src/BJPlayer.cpp
+++ src/BJPlayer.cpp
@@ -1,11 +1,23 @@
 #include "BJPlayer.hpp"
+#include "BlackJackValue.hpp"
 
 int BJPlayer::calculateHandValue() const
 {
     int handValue{};
+    int assNumber{};
 
     for (const auto& card : hand) {
+        if (card.isItAss()) {
+            assNumber++;
+        }
+
         handValue += card.getValue();
+    }
+
+    constexpr int assBenefit{9};
+    while (assNumber > 0 and handValue + assBenefit <= BlackJackValue) {
+        handValue += assBenefit;
+        assNumber--;
     }
 
     return handValue;
