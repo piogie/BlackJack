4303a78f0a39d1ce5c09609083cbe558136f98ce
Piotr Grzelczak <piotr.karol.grzelczak@gmail.com>
2020-02-22 23:17:25 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+#include "CautionPlayer.hpp"
+#include "ClassicCroupier.hpp"
+#include "FileDeckGenerator.hpp"
+#include "Game.hpp"
+
 int main()
 {
     std::cout << "BlackJack" << std::endl;
+
+    CautionPlayer cautionPlayer;
+    ClassicCroupier classicCroupier;
+    FileDeckGenerator deckGenerator{std::ifstream{"test/exampleDecks/exampleDeck.txt"}};
+    Game game{cautionPlayer, classicCroupier, deckGenerator.getDeck()}; // TODO check if it's rvalue
+
+    game.play();
 }
